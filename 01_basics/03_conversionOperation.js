d84a3faff0f367cdaf137a7107428d36184445af
Vikash-Dagar <dagar8138@gmail.com>
2025-01-31 22:46:30 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,19 @@ let number=34
 let numberString=String(number)
 console.log(numberString)
 console.log(typeof numberString)
+//*************************************Operation************************************/
+//2+2,20/2  ->operations
+let value=34
+let negvalue=-(value)
+console.log(negvalue)
+console.log(3**3) // calculate the power of number 3^3 ->9
+console.log(6/3)
+
+let str1 ="Vikash   "
+let str2 ="Dagar"
+let str3 =str1+str2       // similaraly other datatypes can be done
+console.log(str3)
+console.log("1  " + 5)
+console.log("1  "+5 +5)     // similaraly other datatypes can be done
+console.log(5+5+ "  1")
+console.log(2+2/2*3)        //2/2=1, 1*3=3, 3+2=5
