dc641e895bd25f1f1acff4aa2451eea3f7a11b3c
Vikash-Dagar <dagar8138@gmail.com>
2025-01-31 21:03:40 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //this line will treat all JS code asnewer versions of JS
+//alert(3+3)  this will print ans in browser but not in here Nodejs or vscode
+
+//number :1 to2^53
+//bigint :very large values
+//string
+//boolean :true/false
+//null    :assign a empty value
+//undefined : no value is assigned
+let age=20;
+let name="vikash";
+let loggedin=true;
+let marks
+let number=null
+console.log(typeof age)    //"typeof": help to check the type of datatype of given input
+console.log(typeof name)        //datatype:string
+console.log(typeof marks)  //datatype:undefined
+console.log(typeof number)      //datatype :object
