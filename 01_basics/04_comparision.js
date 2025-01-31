3e38055fdb65453e93d37ca7ff21d51a5cdd58f7
Vikash-Dagar <dagar8138@gmail.com>
2025-01-31 23:16:42 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+console.log(2>1)    //here focus on datatype should be same for not getting error
+console.log(56<=5)
+// console.log(4<=1)
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)              //avoid these type of comparisions as they make confusions.
+// console.log(undefined>0)
+// console.log(undefined>=0)
+
+// console.log("2"==2)     //TRUE, == only check the value of no.
+// console.log("2"===2)    //FALSE ===this checks both value and datatype aswell
