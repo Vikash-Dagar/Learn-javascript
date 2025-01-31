957fa40f87e7d71121f43af733be8fa17c7da863
Vikash-Dagar <dagar8138@gmail.com>
2025-01-31 23:46:15 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,18 @@
+// JavaScript is a dynamic language, meaning that the type of a
+//  variable is assigned at runtime based on its value
+//datatype 2 types
+//PRIMITIVE :    these datatype make a copy of their during passing.
+// 7 TYPES // number,string,boolean,null,bigint,undefined,symbol
+let score=10.1
+let name="vikash"
+let id=true
+let temp=null
+let bigint=19213123138n
+let marks;
+let id1=Symbol('123')
+let id2=Symbol('123')
+console.log(id1===id2);
+
+
+//REFERENCE or non-primitive datatype
+//array,objects,functions
