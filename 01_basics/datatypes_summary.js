739aa157c2a36d195f16bf5ea6ca946f532c912a
Vikash-Dagar <dagar8138@gmail.com>
2025-02-01 08:35:11 +0000

    summary of datatypes

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -1,8 +1,8 @@
-// JavaScript is a dynamic language, meaning that the type of a
+// JavaScript is a "dynamic keytype language", meaning that the type of a
 //  variable is assigned at runtime based on its value
 //datatype 2 types
 //PRIMITIVE :    these datatype make a copy of their during passing.
-// 7 TYPES // number,string,boolean,null,bigint,undefined,symbol
+// 7 TYPES // number,string,boolean,null,bigint,undefined,symbol(it is used to make a value unique)
 let score=10.1
 let name="vikash"
 let id=true
@@ -14,5 +14,25 @@ let id2=Symbol('123')
 console.log(id1===id2);
 
 
-//REFERENCE or non-primitive datatype
+//REFERENCE or non-primitive datatype (call by reference)
 //array,objects,functions
+//ARRAY
+let arr = ["truck","car","bus","bike"]
+
+//objects
+{       // any value inside curly brakets is a object wheather it is a 
+       // function,another object,array,number,string
+    name:"vikash"
+    age:21
+}
+let myObj={           // "we can also put a object in a variable"
+    name:"abc",
+    age:30,
+}
+
+//functions:        function(){}   //fxn defination
+ 
+const myFxn=function(){        //we can treat a fxn as a variable in JS. 
+    console.log("Hello Vikash")
+} 
+console.log(typeof myFxn)
